4e067cb2886d8e91e63a342c1cf77c61268f8cfa
othmane <othmaneouacrim@gmail.com>
2024-02-29 15:10:45 +0100

    Initial commit

---
+++ CrudOperationInNetCore/Models/BrandContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudOperationInNetCore.Models
+{
+    public class BrandContext : DbContext
+    {
+
+        public BrandContext(DbContextOptions<BrandContext> options)
+            : base(options) { }
+
+        public DbSet<Brand> Brands { get; set; }
+
+
+
+
+
+
+
+
+    }
+}
